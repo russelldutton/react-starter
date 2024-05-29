bdd95e8656b05926928d3befe3b4f39ede6b5525
Russell <rdutton1504@gmail.com>
2024-05-29 20:38:19 +0000

    Imports

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import { AppLayout } from 'components/AppLayout';
-import { Home } from 'pages/Home';
-import { NotFound } from 'pages/NotFound';
+import { Home } from 'components/pages/Home';
+import { NotFound } from 'components/pages/NotFound';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
